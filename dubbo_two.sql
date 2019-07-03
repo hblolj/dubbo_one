f0f889f933a7a8f8495fc6e8780d158f2455a1d4
hblolj <hblolj@gmail.com>
2019-07-03 17:53:43 +0800

    add sql file

---
+++ dubbo_two.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : local
+Source Server Version : 50717
+Source Host           : localhost:3306
+Source Database       : dubbo_two
+
+Target Server Type    : MYSQL
+Target Server Version : 50717
+File Encoding         : 65001
+
+Date: 2019-07-03 17:52:54
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
